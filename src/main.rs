f6a53c5a7aeeff94bb7b57d0b84aa164d4dee8d9
Nereuxofficial <benediktb11@tuta.io>
2021-08-15 22:25:57 +0200

    Comments added

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ mod tests {
     #[test]
     fn first_crash() {
         // Probably irrelevant
+        // Expected binary operator but there was none
         let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.1a404299a5.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
         println!("{:?}", fuzz);
         eval_str(fuzz.as_str());
@@ -28,12 +29,14 @@ mod tests {
 
     #[test]
     fn second_crash() {
+        // Probably also irrelevant(ParseFloatError)
         let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.e4a70c637.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
         println!("{:?}", fuzz);
         eval_str(fuzz.as_str());
     }
     #[test]
     fn third_crash() {
+        // attempt to add with overflow
         let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.e543c0111.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
         println!("{:?}", fuzz);
         eval_str(fuzz.as_str());
