d0e66b18d30b9b1326a454758f158877889b9f0c
Nereuxofficial <benediktb11@tuta.io>
2021-08-15 21:09:42 +0200

    More fuzzing

--- src/main.rs
+++ src/main.rs
@@ -21,20 +21,20 @@ mod tests {
     #[test]
     fn first_crash() {
         // Probably irrelevant
-        let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.1a41fbdb78.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
+        let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.1a404299a5.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
         println!("{:?}", fuzz);
         eval_str(fuzz.as_str());
     }
 
     #[test]
     fn second_crash() {
-        let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.f566d96db.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
+        let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.e4a70c637.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
         println!("{:?}", fuzz);
         eval_str(fuzz.as_str());
     }
     #[test]
     fn third_crash() {
-        let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.f482151fd.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
+        let fuzz = std::fs::read_to_string("hfuzz_workspace/Fuzzing/SIGABRT.PC.7ffff7c67d22.STACK.e543c0111.CODE.-6.ADDR.0.INSTR.mov____0x108(%rsp),%rax.fuzz").unwrap();
         println!("{:?}", fuzz);
         eval_str(fuzz.as_str());
     }
